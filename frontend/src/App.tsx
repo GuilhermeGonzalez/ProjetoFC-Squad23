5a8b90bbda585c3edc7868e13bde167485b14b88
Guilherme C Gonzalez <gcgonzalez99@hotmail.com>
2021-03-24 16:15:18 -0300

    build: foi adicionado typescript e styled-components

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = (): JSX.Element => {
+    return (
+        <div >
+            Projeto FCamara - SQUAD 23
+        </div>
+    );
+};
+
+export default App;
