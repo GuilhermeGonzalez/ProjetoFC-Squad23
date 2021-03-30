21e2f545a5fb30b86c3c8fa8b0244c8ded9e7918
Guilherme C Gonzalez <gcgonzalez99@hotmail.com>
2021-03-30 11:05:09 -0300

    style: componente footer criado

--- client/src/pages/client/home/index.js
+++ client/src/pages/client/home/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import Header from '../../../components/Header';
+import Footer from '../../../components/Footer';
 
 export default function Home() {
   return (
     <div>
+      <Header />
       <h1>Página Inicial</h1>
+      <Footer />
     </div>
   );
 }
