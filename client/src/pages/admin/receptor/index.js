30b1a6091b2c7fc1564bbfa25be1a5c81611defd
Guilherme C Gonzalez <gcgonzalez99@hotmail.com>
2021-03-31 16:00:06 -0300

    refactor: adição do header/footer

--- client/src/pages/admin/receptor/index.js
+++ client/src/pages/admin/receptor/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import HeaderLogado from '../../../components/HeaderLogado';
+import Footer from '../../../components/Footer';
 
 export default function ReceptorListagem() {
   return (
     <div>
+      <HeaderLogado />
       <h1>Listagem de Receptores</h1>
+      <Footer />
     </div>
   );
 }
