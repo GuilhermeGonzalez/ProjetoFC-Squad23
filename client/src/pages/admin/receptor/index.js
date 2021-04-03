982f2f446a3f20f7cf7cd323c3181f2750d179fd
Guilherme C Gonzalez <gcgonzalez99@hotmail.com>
2021-04-03 15:36:44 -0300

    feat: funcionamento das novas api no perfil do usuário

--- client/src/pages/admin/receptor/index.js
+++ client/src/pages/admin/receptor/index.js
@@ -20,7 +20,17 @@ export default function ReceptorListagem() {
   const [cidade, setCidade] = useState("");
   const [uf, setUf] = useState("");
   const [historia, setHistoria] = useState("");
+
   const [dadosBancarios, setDadosBancarios] = useState({});
+  const [banco, setBanco] = useState("");
+  const [agencia, setAgencia] = useState(0);
+  const [conta_corrente, setContaCorrente] = useState("");
+
+
+  const [edicaoHistoria, setEdicaoHistoria] = useState(false);
+  const [edicaoBanco, setEdicaoBanco] = useState(false);
+  const [destaqueEdicaoHistoria, setDestaqueEdicaoHistoria] = useState({})
+  const [destaqueEdicaoBanco, setDestaqueEdicaoBanco] = useState({})
 
   useEffect(() => {
     async function findInfo() {
@@ -33,10 +43,55 @@ export default function ReceptorListagem() {
       await setCidade(receptor.cidade_rcpt);
       await setUf(receptor.uf_rcpt);
       await setHistoria(receptor.historia_rcpt);
-      await setDadosBancarios(receptor.dados_bancarios_rcpt);
+      await setBanco(receptor.dados_bancarios_rcpt.banco);
+      await setAgencia(receptor.dados_bancarios_rcpt.agencia);
+      await setContaCorrente(receptor.dados_bancarios_rcpt.conta_corrente);
     }
     findInfo();
   }, [])
+
+
+  function habilitaEdicaoHistoria() {
+    setEdicaoHistoria(true);
+    setDestaqueEdicaoHistoria({ border: "3px #000 solid" });
+  }
+
+  function habilitaEdicaoBanco() {
+    setEdicaoBanco(true);
+    setDestaqueEdicaoBanco({ border: "3px #000 solid" });
+  }
+
+  async function salvarAlteracoesHistoria() {
+    setEdicaoHistoria(false);
+    let data = {
+      _id: idReceptor,
+      historia_rcpt: historia
+    }
+    await api.put('/api/receptor.historia', data);
+    setDestaqueEdicaoHistoria({ border: "none" });
+  }
+
+  async function salvarAlteracoesBanco() {
+    setEdicaoBanco(false);
+    let data = {
+      _id: idReceptor,
+      dados_bancarios_rcpt: {
+        banco,
+        agencia,
+        conta_corrente
+      }
+    }
+    await api.put('/api/receptor.dadosBancarios', data);
+    setDestaqueEdicaoBanco({ border: "none" });
+  }
+
+  const BotaoSalvarHistoria = () => (
+    <button onClick={salvarAlteracoesHistoria} className="botaoSalvar">Salvar</button>
+  )
+
+  const BotaoSalvarBanco = () => (
+    <button onClick={salvarAlteracoesBanco} className="botaoSalvar">Salvar</button>
+  )
 
   return (
     <div>
@@ -47,10 +102,7 @@ export default function ReceptorListagem() {
           <div className="cardPerfil">
             <div className="tituloCardPerfil">
               <h2>{nome}</h2>
-              <div className="editarPerfil">
-                <img src={pencilImage} alt="imagem editar" />
-                <p>Editar</p>
-              </div>
+
             </div>
 
             <div className="infoCard">
@@ -75,12 +127,13 @@ export default function ReceptorListagem() {
               <h2>Minha História</h2>
               <div className="editarPerfil">
                 <img src={pencilImage} alt="imagem editar" />
-                <p>Editar</p>
+                <p onClick={habilitaEdicaoHistoria}>Editar</p>
               </div>
             </div>
-            <p>"{historia}"</p>
-
-
+            <p contentEditable={edicaoHistoria} style={destaqueEdicaoHistoria} onInput={(e) => setHistoria(e.target.innerText)}>{historia}</p>
+            <div className="salvarDiv">
+              {edicaoHistoria ? <BotaoSalvarHistoria /> : null}
+            </div>
           </div>
 
 
@@ -89,13 +142,16 @@ export default function ReceptorListagem() {
               <h2>Dados Bancários</h2>
               <div className="editarPerfil">
                 <img src={pencilImage} alt="imagem editar" />
-                <p>Editar</p>
+                <p onClick={habilitaEdicaoBanco}>Editar</p>
               </div>
             </div>
             <div className="infoBanco">
-              <p>Banco {dadosBancarios.banco}</p>
-              <p>Agencia {dadosBancarios.agencia}</p>
-              <p>Conta corrente {dadosBancarios.conta_corrente}</p>
+              <div className="linhaInfoBanco">Banco: <p contentEditable={edicaoBanco} style={destaqueEdicaoBanco} onInput={(e) => setBanco(e.target.innerText)} >{banco}</p></div>
+              <div className="linhaInfoBanco">Agencia: <p contentEditable={edicaoBanco} style={destaqueEdicaoBanco} onInput={(e) => setAgencia(e.target.innerText)}>{agencia}</p></div>
+              <div className="linhaInfoBanco">Conta corrente: <p contentEditable={edicaoBanco} style={destaqueEdicaoBanco} onInput={(e) => setContaCorrente(e.target.innerText)}>{conta_corrente}</p></div>
+            </div>
+            <div className="salvarDiv">
+              {edicaoBanco ? <BotaoSalvarBanco /> : null}
             </div>
           </div>
         </div>
