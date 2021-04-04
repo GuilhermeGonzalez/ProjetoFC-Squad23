89d5ad614c5983ebd62752049f1be0f0b2c98cf3
Guilherme C Gonzalez <gcgonzalez99@hotmail.com>
2021-04-04 12:01:58 -0300

    feat: funcionamento da apid e busca de filtros na home

--- client/src/pages/client/home/index.js
+++ client/src/pages/client/home/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../../../services/api';
 
 import Header from '../../../components/Header';
 import Footer from '../../../components/Footer';
@@ -12,6 +13,34 @@ import HeartImage from '../.././../assets/heart.png'
 import FolhaImage from '../.././../assets/folha.png'
 
 export default function Home() {
+  const [doacao, setDoacao] = useState([{}]);
+  const [tipo_rcpt, setTipo] = useState("");
+  const [uf_rcpt, setUf] = useState("");
+  const [valor_rcpt, setValor] = useState(0);
+
+  useEffect(() => {
+    findAndGenerate();
+  }, [])
+
+  useEffect(() => {
+    findAndGenerate();
+  }, [tipo_rcpt, uf_rcpt, valor_rcpt])
+
+  async function findAndGenerate() {
+    // const { data: receptores } = await api.get('/api/receptor.findReceptor/', data)
+    // await setDoacao(receptores);
+    // console.log(doacao);
+    await api.get('/api/receptor.findReceptor', {
+      params: {
+        tipo: tipo_rcpt,
+        uf: uf_rcpt,
+        valor: valor_rcpt
+      }
+    }).then(res => {
+      setDoacao(res.data);
+    })
+  }
+
   return (
     <div>
       <Header />
@@ -70,7 +99,7 @@ export default function Home() {
             <h1>Buscar doações</h1>
           </div>
           <div className="search-filterBox">
-            <select name="tipo" id="tipo">
+            <select value={tipo_rcpt} onChange={e => setTipo(e.target.value)} name="tipo" id="tipo">
               <option value="" disabled selected hidden>Tipo de usuário</option>
               <option value={"Professor(a)"}>Professor(a)</option>
               <option value={"Pais ou responsáveis"}>Pais ou responsáveis</option>
@@ -78,14 +107,14 @@ export default function Home() {
               <option value={"Estudante Curso Preparatório"}>Estudante Curso Preparatório</option>
             </select>
 
-            <select name="uf">
+            <select value={uf_rcpt} onChange={e => setUf(e.target.value)} name="uf">
               <option value="" disabled selected hidden>Região</option>
               <option value="BH">Bahia</option>
               <option value="RJ">Rio de Janeiro</option>
               <option value="SP">São Paulo</option>
             </select>
 
-            <select name="valor">
+            <select value={valor_rcpt} onChange={e => setValor(e.target.value)} name="valor">
               <option value={0} disabled selected hidden>Valor máximo</option>
               <option value={250}>Até R$250,00</option>
               <option value={500}>Até R$500,00</option>
@@ -96,24 +125,34 @@ export default function Home() {
           </div>
 
           <div className="search-displayBox">
-            <div className="search-cardReceptor">
-              <p>Maria José</p>
-              <div className="search-infoReceptor">
-                <img src={CirculoVazio} alt="" />
-                <div className="info">
-                  <p>Professora de Educação Infantil</p>
-                  <p>Escola Pública</p>
-                  <p>São Paulo, Sp</p>
-                  <p>Doação em dinheiro Disponível</p>
+            {doacao.map(item => {
+              //debugger
+              let porcentage = 0;
+              if (item != {} && item.lista_materiais != undefined) {
+                porcentage = parseFloat(((item.lista_materiais.valorArrecadado / item.lista_materiais.meta) * 100).toFixed(2));
+              }
+
+              return (
+                <div key={item._id} className="search-cardReceptor">
+                  <p>{item.nome_rcpt}</p>
+                  <div className="search-infoReceptor">
+                    <img src={CirculoVazio} alt="" />
+                    <div className="info">
+                      <p>{item.tipo_rcpt}</p>
+                      <p>{item.nivel_rcpt}</p>
+                      <p>{item.cidade_rcpt}, {item.uf_rcpt}</p>
+                      <p>Doação em dinheiro Disponível</p>
+                    </div>
+                  </div>
+                  <div className="search-porcentContainer">
+                    <p>{porcentage} %</p>
+                    <div className="porcentageBar">
+                      <div className="concludedBar"></div>
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <div className="search-porcentContainer">
-                <p>X %</p>
-                <div className="porcentageBar">
-                  <div className="concludedBar"></div>
-                </div>
-              </div>
-            </div>
+              )
+            })}
           </div>
         </div>
       </div>
